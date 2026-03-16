6aac76e30dd10063926789cd197f6d86ba1e8905
Richard Le <169632088+rlechuong@users.noreply.github.com>
2026-03-16 18:35:41 -0400

    Build and style Navbar component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,45 @@
-function Navbar() {
-  return <nav>Navbar</nav>;
+import { Link, NavLink } from "react-router-dom";
+import PropTypes from "prop-types";
+import styles from "./Navbar.module.css";
+
+function Navbar({ cart }) {
+  const cartCount = cart.reduce((total, item) => total + item.quantity, 0);
+
+  return (
+    <nav className={styles.navbar}>
+      <Link to="/" className={styles.logo}>
+        TempShop
+      </Link>
+      <ul className={styles.navLinks}>
+        <li>
+          <NavLink to="/" className={({ isActive }) => (isActive ? styles.active : "")}>
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/shop" className={({ isActive }) => (isActive ? styles.active : "")}>
+            Shop
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/cart"
+            className={({ isActive }) =>
+              isActive ? `${styles.cartLink} ${styles.active}` : styles.cartLink
+            }
+          >
+            Cart ({cartCount})
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
 }
+
+Navbar.propTypes = {
+  cart: PropTypes.arrayOf(
+    PropTypes.shape({ id: PropTypes.number.isRequired, quantity: PropTypes.number.isRequired }),
+  ).isRequired,
+};
 
 export default Navbar;
